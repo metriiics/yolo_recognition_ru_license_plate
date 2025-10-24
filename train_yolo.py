6d698e8472fb4c278361a468c989bfa990d34160
Meltreex <alexseykochetkov23@gmail.com>
2025-10-24 23:45:35 +1000

    Подготовлены данные для обучения yolo

--- train_yolo.py
+++ train_yolo.py
@@ -1,11 +1,9 @@
 from ultralytics import YOLO
 
-# Загружаем базовую YOLOv8n модель
 model = YOLO("yolov8n.pt")
 
-# Обучаем на наших данных
 model.train(
-    data="data/data.yaml",
+    data="data.yaml",
     epochs=50,
     imgsz=640,
     batch=16,
